96e395830186c79ce649c82d290b52cdd56ff3f9
Ahmet TOPAK <75997899+ahmettopak@users.noreply.github.com>
2023-10-01 22:41:31 +0300

    Add comment

---
+++ readme.md
@@ -0,0 +1,55 @@
+Tabii, işte İngilizce bir README dosyası örneği:
+
+---
+
+# Person Query Application
+
+This React application allows users to input various personal details (name, surname, TC number, date of birth, parents' names and TC numbers, as well as address information) and submit a query. When the form is submitted, it sends a GET request to a server endpoint with the form data as parameters. The response from the server, an array of user data, is displayed in a table below the form.
+
+## How to Use
+
+1. Clone the application to your local environment:
+
+```
+git clone <repository-link>
+cd <repository-folder>
+```
+
+2. Install the required dependencies by running the following command in your terminal:
+
+```
+npm install
+```
+
+3. Start the server:
+
+```
+npm start
+```
+
+4. Go to [http://localhost:3000](http://localhost:3000) in your web browser.
+
+5. Fill out the required personal details in the form and submit the query by clicking the "Submit" button.
+
+6. The data received from the server will be displayed in a table below the form.
+
+## Technologies Used
+
+- **React:** Used for the user interface and component management.
+- **Axios:** Used for making HTTP requests to the server.
+
+## Main Files and Folders
+
+- **App.js:** Main application component.
+- **UserData.jsx:** Component used to display user data in a table.
+- **components/:** Folder containing application components.
+- **README.md:** This file; provides information on how to run the application, the technologies used, and other important details.
+
+## Important Notes
+
+- Ensure that the server application (for example: http://localhost:3000) is running.
+- The application requires an internet connection to receive data from the server after submitting the form.
+
+---
+
+This README file provides comprehensive guidance to users on how to use the application, the technologies used, and other essential information. It includes details on how to run the application and the steps to be followed.
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import { useEffect, useState } from "react";
-import UserData from "./components/UserData.jsx";
-import axios from 'axios';
+// Importing necessary modules and components from external files
+import { useEffect, useState } from "react"; // Importing React hooks for managing state and side effects
+import UserData from "./components/UserData.jsx"; // Importing the UserData component from an external file
+import axios from 'axios'; // Importing Axios for making HTTP requests
 
+// App component definition
 const App = () => {
-    const [users, setUsers] = useState([]);
-    const [formData, setFormData] = useState({
+    // State variables initialization using the useState hook
+    const [users, setUsers] = useState([]); // State variable to store user data fetched from the server
+    const [formData, setFormData] = useState({ // State variable to store form data entered by the user
         name: '',
         surname: '',
         tc: '',
@@ -17,65 +20,74 @@ const App = () => {
         fathertc: ''
     });
 
+    // Function to handle form submission
     const handleSubmit = async (e) => {
-        e.preventDefault();
+        e.preventDefault(); // Prevents the default form submission behavior
         try {
-            setUsers([]);
+            setUsers([]); // Clears the users state to prepare for new data
+            // Sends a GET request to the server with form data as parameters
             const response = await axios.get('http://localhost:3000/person', { params: formData });
-            console.log('Server Response:', response.data);
+            console.log('Server Response:', response.data); // Logs the server response data to the console
+            // If the response contains data, updates the users state with the received data
             if (response.data.length > 0) {
                 setUsers(response.data);
             }
         } catch (error) {
-            console.error('Error:', error);
+            console.error('Error:', error); // Logs any errors that occur during the API request
         }
     };
 
+    // Function to handle input changes in the form fields
     const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value });
+        const { name, value } = e.target; // Destructures the name and value properties from the input element
+        setFormData({ ...formData, [name]: value }); // Updates the form data state with the new input value
     };
+
+    // JSX structure representing the form and user data table
     return <>
-
+        {/* Top-level div element with a class name */}
         <div className="App">
-            <h1>Kişi Sorgulama</h1>
-
+            <h1>Kişi Sorgulama</h1> {/* Heading for the form */}
+            {/* Form element with input fields and event handlers */}
             <form onSubmit={handleSubmit}>
-
+                {/* Input fields for various user data */}
+                {/* Each input field is associated with a specific property in the form data state */}
                 <label>Ad:</label>
                 <input type="text" name="name" value={formData.name} onChange={handleInputChange} /><br />
-
+                {/* Input field for user's surname */}
                 <label>Soyad:</label>
                 <input type="text" name="surname" value={formData.surname} onChange={handleInputChange} /><br />
-
+                {/* Input field for user's TC (Türkiye Cumhuriyeti) number */}
                 <label>TC:</label>
                 <input type="text" name="tc" value={formData.tc} onChange={handleInputChange} /><br />
-
+                {/* Input field for user's date of birth */}
                 <label>Doğum Tarihi:</label>
                 <input type="text" name="dob" value={formData.dob} onChange={handleInputChange} /><br />
-
+                {/* Input field for user's mother's name */}
                 <label>Anne adı:</label>
                 <input type="text" name="mothername" value={formData.mothername} onChange={handleInputChange} /><br />
-
+                {/* Input field for user's father's name */}
                 <label>Baba Adı</label>
                 <input type="text" name="fathername" value={formData.fathername} onChange={handleInputChange} /><br />
-
+                {/* Input field for user's mother's TC number */}
                 <label>Anne TC:</label>
                 <input type="text" name="mothertc" value={formData.mothertc} onChange={handleInputChange} /><br />
-
+                {/* Input field for user's father's TC number */}
                 <label>Baba TC:</label>
                 <input type="text" name="fathertc" value={formData.fathertc} onChange={handleInputChange} /><br />
-
+                {/* Input field for user's province */}
                 <label>Nufus İl:</label>
                 <input type="text" name="province" value={formData.province} onChange={handleInputChange} /><br />
-
+                {/* Input field for user's detailed address within the province */}
                 <label>Nufus İlçe:</label>
                 <input type="text" name="provincedetail" value={formData.provincedetail} onChange={handleInputChange} /><br />
-
+                {/* Submit button to trigger the form submission */}
                 <button type="submit">Submit</button>
             </form>
+            {/* Table to display user data */}
             <table>
                 <thead>
+                    {/* Table header row with column names */}
                     <tr>
                         <th>ADI</th>
                         <th>SOYADI</th>
@@ -90,13 +102,12 @@ const App = () => {
                     </tr>
                 </thead>
                 <tbody>
+                    {/* Rendering the UserData component and passing the users data as a prop */}
                     <UserData users={users} />
                 </tbody>
             </table>
-        </div>
-
+        </div> {/* Closing div for the main container */}
     </>
 }
 
-export default App;
-
+export default App; // Exporting the App component for use in other parts of the application
